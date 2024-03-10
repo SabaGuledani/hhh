6061c96f53ad04bde6fbe0f310423cb7c377c8e8
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-03-10 17:04:24 +0400

    Added signal emitting, reorganised file structure

--- NewspaperGame/newspaperGame.gd
+++ NewspaperGame/newspaperGame.gd
@@ -5,6 +5,7 @@ var speed = 0.35
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Path2D/PathFollow2D/Natsarqeqia/AnimatedSprite2D.play("move")
+	NewspaperState.showQuestionImages.connect(on_question_image_show)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,9 +14,7 @@ func _process(delta):
 		$Path2D/PathFollow2D.progress_ratio += delta * speed
 	else:
 		$Path2D/PathFollow2D/Natsarqeqia/AnimatedSprite2D.animation = "default"
-		
-	
 
 
-
-
+func on_question_image_show(questionNumber):
+	print("signal_emitted" + str(questionNumber))
