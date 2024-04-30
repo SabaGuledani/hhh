fc2050ff5bc2bc96b8d1780c04dde01c0a213bd0
saba_guledani <115211537+SabaGuledani@users.noreply.github.com>
2024-04-30 12:45:06 +0400

    Added more menu buttons

--- Menu/mainGame.gd
+++ Menu/mainGame.gd
@@ -1,5 +1,8 @@
 extends Control
 
+@onready var mode_btn_control = $ModeChoose
+@onready var start_control = $Start
+@onready var miniGameChooseMenu = $MiniGameChooseMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,9 +14,33 @@ func _process(delta):
 	pass
 
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://GameChoose.tscn")
-
 
 func _on_start_button_pressed():
-	pass # Replace with function body.
+	mode_btn_control.visible = true
+	start_control.visible = false
+	
+
+
+func _on_story_mode_button_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_choose_game_button_pressed():
+	mode_btn_control.visible = false
+	miniGameChooseMenu.visible = true
+
+
+func _on_tyis_feria_button_pressed():
+	get_tree().change_scene_to_file("res://feria.tscn")
+
+
+func _on_iluziis_gamovlena_button_pressed():
+	get_tree().change_scene_to_file("res://NewspaperGame/newspaperGame.tscn")
+
+
+func _on_gnomi_button_pressed():
+	get_tree().change_scene_to_file("res://GnomeGame/gnome_encounter.tscn")
+
+
+func _on_dakarguli_mefe_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/king_encounter.tscn")
