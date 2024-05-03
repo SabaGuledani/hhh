63f68346c0bf0504491849b3c49bcc395d9f9085
saba_guledani <115211537+SabaGuledani@users.noreply.github.com>
2024-05-03 18:04:35 +0400

    Added back button

--- Menu/mainGame.gd
+++ Menu/mainGame.gd
@@ -3,20 +3,22 @@ extends Control
 @onready var mode_btn_control = $ModeChoose
 @onready var start_control = $Start
 @onready var miniGameChooseMenu = $MiniGameChooseMenu
-
+@onready var backButton = $BackButton
 signal gamePressed(gameName)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	backButton.visible = false
 	$TileMap/Natsarqeqia/AnimatedSprite2D.flip_h = true
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if start_control.visible == true:
+		backButton.visible = false
 
 
 
 func _on_start_button_pressed():
+	backButton.visible = true 
 	mode_btn_control.visible = true
 	start_control.visible = false
 	
@@ -46,3 +48,12 @@ func _on_gnomi_button_pressed():
 func _on_dakarguli_mefe_button_pressed():
 #	get_tree().change_scene_to_file("res://Scenes/king_encounter.tscn")
 	gamePressed.emit("king")
+
+
+func _on_button_pressed():
+	if mode_btn_control.visible == true:
+		mode_btn_control.visible = false
+		start_control.visible = true
+	elif miniGameChooseMenu.visible == true:
+		miniGameChooseMenu.visible = false
+		mode_btn_control.visible = true
